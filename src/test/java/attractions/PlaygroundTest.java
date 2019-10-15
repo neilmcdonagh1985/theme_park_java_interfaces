32eb46b460aec80d80c15471eb43f6f58ede0ff7
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-10-15 19:27:38 +0100

    stall tests done

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -9,11 +9,13 @@ import static org.junit.Assert.assertEquals;
 public class PlaygroundTest {
     Playground playground;
     Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
         visitor = new Visitor(14, 5.8, 5.0);
+        visitor2 = new Visitor(16, 5.11, 10.00);
     }
 
     @Test
@@ -34,5 +36,10 @@ public class PlaygroundTest {
     @Test
     public void ifIsAllowedTo() {
         assertEquals(false, playground.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void ifNotAllowedTo() {
+        assertEquals(true, playground.isAllowedTo(visitor2));
     }
 }
--- src/test/java/stalls/TobaccoStallTest.java
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,22 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor = new Visitor(17, 5.5, 15.0);
+        visitor2 = new Visitor(18, 5.11, 30.0);
+
     }
 
     @Test
@@ -27,5 +33,15 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void canAllowTobacco() {
+        assertEquals(false, tobaccoStall.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void canRefuseTobacco() {
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor2));
     }
 }
